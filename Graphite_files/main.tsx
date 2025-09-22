9fa33520e4b37292a425c757e18b59559e1f7dfb
George Tao <yultao@gmail.com>
2025-09-21 20:59:36 -0400

    Updates

---
+++ Graphite_files/main.tsx
@@ -0,0 +1,12 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=a4d663c7"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=5b81ed11"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?t=1758379280128";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "C:/Workspace/graphite/src/main.tsx",
+    lineNumber: 5,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBSUk7QUFKSixTQUFTQSxrQkFBa0I7QUFDM0IsT0FBT0MsU0FBUztBQUVoQkQsV0FBV0UsU0FBU0MsZUFBZSxNQUFNLENBQUUsRUFBRUM7QUFBQUEsRUFDekMsdUJBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBQUk7QUFDUiIsIm5hbWVzIjpbImNyZWF0ZVJvb3QiLCJBcHAiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcCdcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpISkucmVuZGVyKFxuICAgIDxBcHAgLz5cbilcbiJdLCJmaWxlIjoiQzovV29ya3NwYWNlL2dyYXBoaXRlL3NyYy9tYWluLnRzeCJ9
